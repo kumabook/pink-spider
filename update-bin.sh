468ae933b286b4fdece2c59f359ee6a7e174b862
Hiroki Kumamoto <kumabook@live.jp>
2015-10-01 02:20:17 +0900

    rustup to 1.3.0

--- update-bin.sh
+++ update-bin.sh
@@ -27,19 +27,16 @@ fi
 DATE=`date '+%Y-%m-%d'`
 
 # The names of our nightly tarballs.
-RUST_TARBALL=rust-nightly-x86_64-unknown-linux-gnu.tar.gz
-CARGO_TARBALL=cargo-nightly-x86_64-unknown-linux-gnu.tar.gz
+RUST_TARBALL=rust-1.3.0-x86_64-unknown-linux-gnu.tar.gz
 
 # Download our tarballs.
 echo "-----> Fetching nightly builds"
-rm -f "$RUST_TARBALL" "$CARGO_TARBALL"
+rm -f "$RUST_TARBALL"
 curl -O "https://static.rust-lang.org/dist/$RUST_TARBALL"
-curl -O "https://static.rust-lang.org/cargo-dist/$CARGO_TARBALL"
 
 # Upload our tarballs to S3.
 echo "-----> Uploading to S3"
 aws s3 cp "$RUST_TARBALL" "s3://$BUCKET/$DATE/" --acl public-read
-aws s3 cp "$CARGO_TARBALL" "s3://$BUCKET/$DATE/" --acl public-read
 
 # Updating RustConfig.
 echo "-----> Updating RustConfig"
@@ -47,9 +44,5 @@ cat <<EOF > RustConfig
 URL="https://s3.amazonaws.com/$BUCKET/$DATE/$RUST_TARBALL"
 VERSION="$DATE"
 
-CARGO_URL="https://s3.amazonaws.com/$BUCKET/$DATE/$CARGO_TARBALL"
-CARGO_VERSION="$DATE"
-EOF
-
 echo "-----> Cleaning up"
-rm -f "$RUST_TARBALL" "$CARGO_TARBALL"
+rm -f "$RUST_TARBALL"
