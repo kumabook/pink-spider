9852da7c6cb28b715ff210e8b6ec7e7f264f5184
Hiroki Kumamoto <kumabook@live.jp>
2015-12-19 13:51:32 +0900

    Playlistify api extract tracks from youtube and soundcloud playlist url

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,18 @@
+extern crate hyper;
+extern crate rustc_serialize;
+extern crate html5ever;
+extern crate tendril;
+extern crate regex;
+extern crate string_cache;
+extern crate url;
+#[macro_use]
+extern crate lazy_static;
+
 pub use self::model::Track;
 pub use self::model::Playlist;
 pub use self::model::Provider;
 
 pub mod scraper;
 pub mod model;
+pub mod youtube;
+pub mod soundcloud;
--- src/scraper.rs
+++ src/scraper.rs
@@ -22,13 +22,17 @@ use self::hyper::header::ConnectionOption;
 
 use Provider;
 use Track;
+use soundcloud;
+use youtube;
 
 use self::url::percent_encoding::lossy_utf8_percent_decode;
 
 static YOUTUBE_EMBED:    &'static str = r"www.youtube.com/embed/([^\?&{videoseries}].+)";
-static YOUTUBE_LIST:     &'static str = r"www.youtube.com/embed/videoseries\?list=([^\?]+)";
-static YOUTUBE_WATCH:    &'static str = r"www.youtube.com/watch\?v=([^\?&]+)";
-static SOUNDCLOUD_TRACK: &'static str = r"api.soundcloud.com/tracks/([^\?&]+)";
+static YOUTUBE_LIST:         &'static str = r"www.youtube.com/embed/videoseries\?list=([^\?]+)";
+static YOUTUBE_WATCH:        &'static str = r"www.youtube.com/watch\?v=([^\?&]+)";
+static SOUNDCLOUD_TRACK:     &'static str = r"api.soundcloud.com/tracks/([^\?&]+)";
+static SOUNDCLOUD_PLAYLIST:  &'static str = r"api.soundcloud.com/playlists/([^\?&]+)";
+static SOUNDCLOUD_USER:      &'static str = r"api.soundcloud.com/users/([^\?&]+)";
 
 pub fn extract_tracks(url: &str) -> Vec<Track> {
     let client = Client::new();
@@ -55,14 +59,7 @@ fn walk(indent: usize, handle: Handle, tracks: &mut Vec<Track>) {
         Comment(_)       => (),
         Element(ref name, _, ref attrs) => {
             let tag_name = name.local.as_ref();
-            match extract_track(tag_name, attrs) {
-                Some(track) => {
-                    if !(*tracks).contains(&track) {
-                        (*tracks).push(track)
-                    }
-                },
-                None => {}
-            }
+            (*tracks).append(&mut extract_tracks_from_tag(tag_name, attrs))
         }
     }
     for child in node.children.iter() {
@@ -95,59 +92,97 @@ fn extract_identifier(value: &str, regex_str: &str) -> Option<String> {
     }
 }
 
-pub fn extract_track(tag_name: &str, attrs: &Vec<Attribute>) -> Option<Track> {
+pub fn extract_tracks_from_tag(tag_name: &str,
+                               attrs: &Vec<Attribute>) -> Vec<Track> {
     if tag_name == "iframe" {
         match attr("src", attrs) {
-            Some(ref src) => {
-                let decoded = lossy_utf8_percent_decode(src.as_bytes());
-                match extract_identifier(&decoded, YOUTUBE_EMBED) {
-                    Some(identifier) => {
-                        return Some(Track {
-                                    id: 0,
-                              provider: Provider::YouTube,
-                                 title: "".to_string(),
-                                   url: src.to_string(),
-                            identifier: identifier
-                        });
-                    },
-                    None => ()
-                }
-                match extract_identifier(&decoded, SOUNDCLOUD_TRACK) {
-                    Some(identifier) => {
-                        return Some(Track {
-                                    id: 0,
-                              provider: Provider::SoundCloud,
-                                 title: "".to_string(),
-                                   url: src.to_string(),
-                            identifier: identifier
-                        });
-                    },
-                    None => ()
-                }
-            },
-            None => ()
+            Some(ref src) => extract_tracks_from_url(src.to_string()),
+            None => vec![]
         }
     } else if tag_name == "a" || tag_name == "link" {
         match attr("href", attrs) {
-            Some(ref href) => {
-                let decoded = lossy_utf8_percent_decode(href.as_bytes());
-                match extract_identifier(&decoded, YOUTUBE_WATCH) {
-                    Some(identifier) => {
-                        return Some(Track {
-                                    id: 0,
-                              provider: Provider::YouTube,
-                                 title: "".to_string(),
-                                   url: href.to_string(),
-                            identifier: identifier
-                        });
-                    },
-                    None => ()
-                }
-            },
-            None => ()
+            Some(ref href) => extract_tracks_from_url(href.to_string()),
+            None => vec![]
         }
+    } else {
+        vec![]
     }
-    return None
+}
+
+fn extract_tracks_from_url(url: String) -> Vec<Track> {
+    let decoded = lossy_utf8_percent_decode(url.as_bytes());
+    match extract_identifier(&decoded, YOUTUBE_EMBED) {
+        Some(identifier) => {
+            return vec![Track {
+                        id: 0,
+                  provider: Provider::YouTube,
+                     title: "".to_string(),
+                       url: url.to_string(),
+                identifier: identifier
+            }]
+        },
+        None => ()
+    }
+    match extract_identifier(&decoded, YOUTUBE_WATCH) {
+        Some(identifier) => {
+            return vec![Track {
+                        id: 0,
+                  provider: Provider::YouTube,
+                     title: "".to_string(),
+                       url: url.to_string(),
+                identifier: identifier
+            }]
+        },
+        None => ()
+    }
+    match extract_identifier(&decoded, YOUTUBE_LIST) {
+        Some(identifier) => {
+            return match youtube::fetch_playlist(&identifier) {
+                Ok(res) => res.items.iter()
+                                    .map(|ref i| Track::from_yt_playlist_item(i))
+                                    .collect::<Vec<_>>(),
+                Err(_)       => vec![]
+            }
+        },
+        None => ()
+    }
+    match extract_identifier(&decoded, SOUNDCLOUD_TRACK) {
+        Some(identifier) => {
+            return vec![Track {
+                        id: 0,
+                  provider: Provider::SoundCloud,
+                     title: "".to_string(),
+                       url: url.to_string(),
+                identifier: identifier
+            }]
+        },
+        None => ()
+    }
+    match extract_identifier(&decoded, SOUNDCLOUD_PLAYLIST) {
+        Some(identifier) => {
+            return match soundcloud::fetch_playlist(&identifier) {
+                Ok(playlist) => playlist.tracks
+                                        .iter()
+                                        .map(|ref t| Track::from_sc_track(t))
+                                        .collect::<Vec<_>>(),
+                Err(_)       => vec![]
+            }
+        },
+        None => ()
+    }
+    match extract_identifier(&decoded, SOUNDCLOUD_USER) {
+        Some(identifier) => {
+            print!("user {}", identifier);
+            return match soundcloud::fetch_user_tracks(&identifier) {
+                Ok(tracks) => tracks.iter()
+                                    .map(|ref t| Track::from_sc_track(t))
+                                    .collect::<Vec<_>>(),
+                Err(_)     => vec![]
+            }
+        },
+        None => ()
+    }
+    return vec![]
 }
 
 #[cfg(test)]
