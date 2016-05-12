6e24be85d2c5a7b912571f6e8e6c08bed6816e7f
Hiroki Kumamoto <kumabook@live.jp>
2016-05-12 11:12:30 +0900

    Refactoring: model.rs -> model/mod.rs, model/entry.rs, model/track.rs

---
+++ src/model/mod.rs
@@ -0,0 +1,23 @@
+pub use self::track::{Track, Provider, Playlist};
+pub use self::entry::Entry;
+
+mod track;
+mod entry;
+
+use postgres::{Connection, SslMode};
+use postgres::error::ConnectError;
+use std::env;
+
+
+static DEFAULT_DATABASE_URL: &'static str = "postgres://kumabook@localhost/pink_spider_development_master";
+
+
+pub fn conn() -> Result<Connection, ConnectError> {
+    let opt_url = env::var("DATABASE_URL");
+    match opt_url {
+        Ok(url) =>
+            Connection::connect(url.trim(), SslMode::None),
+        Err(_)  =>
+            Connection::connect(DEFAULT_DATABASE_URL, SslMode::None)
+    }
+}
