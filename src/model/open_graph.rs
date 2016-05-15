c3eba859930fcc8f991def2671dced7178cf44e1
Hiroki Kumamoto <kumabook@live.jp>
2016-05-15 16:42:14 +0900

    Impl ToJson for open_graph::Object

--- src/model/open_graph.rs
+++ src/model/open_graph.rs
@@ -1,3 +1,6 @@
+use std::collections::BTreeMap;
+use rustc_serialize::json::{ToJson, Json};
+
 #[derive(Debug)]
 pub struct Object {
     pub title:            String,
@@ -13,6 +16,30 @@ pub struct Object {
     pub locale:           Option<String>,
     pub locale_alternate: Option<Vec<String>>,
     pub site_name:        Option<String>,
+}
+
+impl ToJson for Object {
+    fn to_json(&self) -> Json {
+        let mut d = BTreeMap::new();
+        d.insert("title".to_string()           , self.title.to_json());
+        d.insert("type".to_string()            , self.obj_type.to_json());
+        d.insert("url".to_string()             , self.url.to_json());
+
+        d.insert("images".to_string(),
+                 Json::Array(self.images.iter().map(|x| x.to_json()).collect()));
+        d.insert("audios".to_string(),
+                 Json::Array(self.audios.iter().map(|x| x.to_json()).collect()));
+        d.insert("videos".to_string(),
+                 Json::Array(self.videos.iter().map(|x| x.to_json()).collect()));
+
+        d.insert("description".to_string()     , self.description.to_json());
+        d.insert("determiner".to_string()      , self.determiner.to_json());
+        d.insert("locale".to_string()          , self.locale.to_json());
+        d.insert("locale_alternate".to_string(), self.locale.to_json());
+        d.insert("site_name".to_string()       , self.locale.to_json());
+
+        Json::Object(d)
+    }
 }
 
 #[derive(Debug)]
@@ -31,6 +58,50 @@ pub enum ObjectType {
     Movie,
     Episode,
     TVShow,
+    VideoOther,
+}
+
+impl ObjectType {
+    fn to_string(&self) -> String {
+        match *self {
+            ObjectType::Article      => "article",
+            ObjectType::Book         => "book",
+            ObjectType::Profile      => "profile",
+            ObjectType::Website      => "website",
+            ObjectType::Song         => "music.song",
+            ObjectType::Album        => "music.album",
+            ObjectType::Playlist     => "music.playlist",
+            ObjectType::RadioStation => "music.radio_station",
+            ObjectType::Movie        => "video.movie",
+            ObjectType::Episode      => "video.episode",
+            ObjectType::TVShow       => "video.tv_show",
+            ObjectType::VideoOther   => "video.other"
+        }.to_string()
+    }
+    pub fn new(str: String) -> ObjectType {
+        match str.as_ref() {
+            "article"             => ObjectType::Article,
+            "book"                => ObjectType::Book,
+            "profile"             => ObjectType::Profile,
+            "website"             => ObjectType::Website,
+            "music.song"          => ObjectType::Song,
+            "music.album"         => ObjectType::Album,
+            "music.playlist"      => ObjectType::Playlist,
+            "music.radio_station" => ObjectType::RadioStation,
+            "video.movie"         => ObjectType::Movie,
+            "video.episode"       => ObjectType::Episode,
+            "video.tv_show"       => ObjectType::TVShow,
+            "video.other"         => ObjectType::VideoOther,
+            _                     => ObjectType::Website,
+        }
+    }
+}
+
+
+impl ToJson for ObjectType {
+    fn to_json(&self) -> Json {
+        self.to_string().to_json()
+    }
 }
 
 #[derive(Debug)]
@@ -42,6 +113,33 @@ pub enum Determiner {
     Auto,
 }
 
+impl Determiner {
+    fn to_string(&self) -> String {
+        match *self {
+            Determiner::A     => "a",
+            Determiner::An    => "an",
+            Determiner::The   => "the",
+            Determiner::Blank => "",
+            Determiner::Auto  => "auto",
+        }.to_string()
+    }
+    pub fn new(str: String) -> Determiner {
+        match str.as_ref() {
+            "a"     => Determiner::A,
+            "an"    => Determiner::An,
+            "the"   => Determiner::The,
+            "auto"  => Determiner::Auto,
+            _       => Determiner::Blank,
+        }
+    }
+}
+
+impl ToJson for Determiner {
+    fn to_json(&self) -> Json {
+        self.to_string().to_json()
+    }
+}
+
 #[derive(Debug)]
 pub struct Image {
     pub url:        String,
@@ -49,6 +147,18 @@ pub struct Image {
     pub obj_type:   Option<String>,
     pub width:      Option<i32>,
     pub height:     Option<i32>
+}
+
+impl ToJson for Image {
+    fn to_json(&self) -> Json {
+        let mut d = BTreeMap::new();
+        d.insert("url".to_string()       , self.url.to_json());
+        d.insert("secure_url".to_string(), self.secure_url.to_json());
+        d.insert("type".to_string()      , self.obj_type.to_json());
+        d.insert("width".to_string()     , self.width.to_json());
+        d.insert("height".to_string()    , self.height.to_json());
+        Json::Object(d)
+    }
 }
 
 #[derive(Debug)]
@@ -60,6 +170,18 @@ pub struct Video {
     pub height:     Option<i32>
 }
 
+impl ToJson for Video {
+    fn to_json(&self) -> Json {
+        let mut d = BTreeMap::new();
+        d.insert("url".to_string()       , self.url.to_json());
+        d.insert("secure_url".to_string(), self.secure_url.to_json());
+        d.insert("type".to_string()      , self.obj_type.to_json());
+        d.insert("width".to_string()     , self.width.to_json());
+        d.insert("height".to_string()    , self.height.to_json());
+        Json::Object(d)
+    }
+}
+
 #[derive(Debug)]
 pub struct Audio {
     pub url:        String,
@@ -67,3 +189,12 @@ pub struct Audio {
     pub obj_type:   Option<String>,
 }
 
+impl ToJson for Audio {
+    fn to_json(&self) -> Json {
+        let mut d = BTreeMap::new();
+        d.insert("url".to_string()       , self.url.to_json());
+        d.insert("secure_url".to_string(), self.secure_url.to_json());
+        d.insert("type".to_string()      , self.obj_type.to_json());
+        Json::Object(d)
+    }
+}
