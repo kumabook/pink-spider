541f9417f3e4555383d2a00cf940d00aa76fba34
Hiroki Kumamoto <kumabook@live.jp>
2016-05-16 23:04:52 +0900

    Add title, description, visual_url, locale members to Entry struct

--- src/model/entry.rs
+++ src/model/entry.rs
@@ -4,26 +4,29 @@ use uuid::Uuid;
 use error::Error;
 use super::conn;
 use Track;
-use super::open_graph;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Entry {
-    pub id:      Uuid,
-    pub url:     String,
-    pub tracks:  Vec<Track>,
-
-    pub og_obj: Option<open_graph::Object>,
+    pub id:          Uuid,
+    pub url:         String,
+    pub title:       Option<String>,
+    pub description: Option<String>,
+    pub visual_url:  Option<String>,
+    pub locale:      Option<String>,
+    pub tracks:      Vec<Track>,
 }
 
 impl ToJson for Entry {
     fn to_json(&self) -> Json {
         let mut d = BTreeMap::new();
-        d.insert("id".to_string(),  self.id.to_string().to_json());
-        d.insert("url".to_string(), self.url.to_json());
-        d.insert("tracks".to_string(),
-                 Json::Array(self.tracks.iter().map(|x| x.to_json()).collect()));
-
-        d.insert("og_object".to_string()  , self.og_obj.to_json());
+        let tracks = Json::Array(self.tracks.iter().map(|x| x.to_json()).collect());
+        d.insert("id".to_string()         , self.id.to_string().to_json());
+        d.insert("url".to_string()        , self.url.to_json());
+        d.insert("title".to_string()      , self.title.to_json());
+        d.insert("description".to_string(), self.description.to_json());
+        d.insert("visual_url".to_string() , self.visual_url.to_json());
+        d.insert("locale".to_string()     , self.locale.to_json());
+        d.insert("tracks".to_string()     , tracks);
         Json::Object(d)
     }
 }
@@ -31,13 +34,16 @@ impl ToJson for Entry {
 impl Entry {
     pub fn find_by_id(id: String) -> Result<Entry, Error> {
         let conn = conn().unwrap();
-        let stmt = conn.prepare("SELECT id, url FROM entries WHERE id = $1").unwrap();
+        let stmt = conn.prepare("SELECT id, url, title, description, visual_url, locale FROM entries WHERE id = $1").unwrap();
         for row in stmt.query(&[&id]).unwrap().iter() {
             return Ok(Entry {
                     id: row.get(0),
                    url: row.get(1),
+                 title: row.get(2),
+           description: row.get(3),
+            visual_url: row.get(4),
+                locale: row.get(5),
                 tracks: Track::find_by_entry_id(row.get(0)),
-                og_obj: None,
             });
         }
         return Err(Error::NotFound)
@@ -45,13 +51,16 @@ impl Entry {
 
     pub fn find_by_url(url: &str) -> Result<Entry, Error> {
         let conn = conn().unwrap();
-        let stmt = conn.prepare("SELECT id, url FROM entries WHERE url = $1").unwrap();
+        let stmt = conn.prepare("SELECT id, url, title, description, visual_url, locale FROM entries WHERE url = $1").unwrap();
         for row in stmt.query(&[&url]).unwrap().iter() {
             return Ok(Entry {
                     id: row.get(0),
                    url: row.get(1),
+                 title: row.get(2),
+           description: row.get(3),
+            visual_url: row.get(4),
+                locale: row.get(5),
                 tracks: Track::find_by_entry_id(row.get(0)),
-                og_obj: None,
             });
         }
         return Err(Error::NotFound)
@@ -59,14 +68,17 @@ impl Entry {
 
     pub fn find_all() -> Vec<Entry> {
         let conn = conn().unwrap();
-        let stmt = conn.prepare("SELECT id, url FROM entries").unwrap();
+        let stmt = conn.prepare("SELECT id, url, title, description, visual_url, locale FROM entries").unwrap();
         let mut entries = Vec::new();
         for row in stmt.query(&[]).unwrap().iter() {
             entries.push(Entry {
                     id: row.get(0),
                    url: row.get(1),
+                 title: row.get(2),
+           description: row.get(3),
+            visual_url: row.get(4),
+                locale: row.get(5),
                 tracks: Track::find_by_entry_id(row.get(0)),
-                og_obj: None,
             })
         }
         return entries
@@ -86,8 +98,11 @@ impl Entry {
             let entry = Entry {
                       id: row.get(0),
                      url: url,
+                   title: None,
+             description: None,
+              visual_url: None,
+                  locale: None,
                   tracks: Vec::new(),
-                  og_obj: None,
             };
             return Ok(entry);
         }
@@ -102,7 +117,16 @@ impl Entry {
         self.tracks.push(track);
     }
 
-    pub fn save(&self) -> bool {
-        return true
+    pub fn save(&self) -> Result<(), Error> {
+        let conn = conn().unwrap();
+        let stmt = conn.prepare("UPDATE entries SET url=$2, title=$3, description=$4, visual_url=$5, locale=$6 WHERE id = $1").unwrap();
+        let result = stmt.query(&[&self.id,
+                                  &self.url,
+                                  &self.title,
+                                  &self.description,
+                                  &self.visual_url,
+                                  &self.locale]);
+        try!(result);
+        Ok(())
     }
 }
