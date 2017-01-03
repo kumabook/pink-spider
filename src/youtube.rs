9dd5360f36b715c4daa207a1144c3bca0e38da94
Hiroki Kumamoto <kumabook@live.jp>
2017-01-03 18:42:32 +0900

    Implement youtube::fetch_video

--- src/youtube.rs
+++ src/youtube.rs
@@ -23,7 +23,7 @@ lazy_static! {
 }
 
 
-#[derive(Debug, RustcDecodable, RustcEncodable)]
+#[derive(Debug, Clone, RustcDecodable, RustcEncodable)]
 pub struct Thumbnail {
     pub url:    String,
     pub width:  i32,
@@ -69,21 +69,63 @@ pub struct PlaylistItemSnippet {
     pub resourceId:    BTreeMap<String, String>
 }
 
+#[allow(non_snake_case)]
+#[derive(Debug, RustcDecodable, RustcEncodable)]
+pub struct VideoResponse {
+    pub kind:          String,
+    pub etag:          String,
+    pub pageInfo:      BTreeMap<String, i32>,
+    pub items:         Vec<Video>,
+}
+
+#[allow(non_snake_case)]
+#[derive(Debug, Clone, RustcDecodable, RustcEncodable)]
+pub struct Video {
+    pub kind:    String,
+    pub etag:    String,
+    pub id:      String,
+    pub snippet: VideoSnippet,
+}
+
+#[allow(non_snake_case)]
+#[derive(Debug, Clone, RustcDecodable, RustcEncodable)]
+pub struct VideoSnippet {
+    pub title:                String,
+    pub description:          String,
+    pub publishedAt:          String,
+    pub channelId:            String,
+    pub channelTitle:         String,
+    pub thumbnails:           BTreeMap<String, Thumbnail>,
+    pub tags:                 Vec<String>,
+    pub categoryId:           String,
+    pub liveBroadcastContent: String,
+}
+
 pub fn fetch_playlist(id: &str) -> json::DecodeResult<PlaylistItemResponse> {
     let params = format!("key={}&part=snippet&playlistId={}", *API_KEY, id);
-    let url    = format!("{}/{}?{}",
-                         BASE_URL,
-                         "playlistItems",
-                         params);
-    println!("{}", url);
+    let url    = format!("{}/{}?{}", BASE_URL, "playlistItems", params);
     let client = Client::new();
     let mut res = client.get(&url)
-        .header(Connection::close())
-        .send().unwrap();
+                        .header(Connection::close())
+                        .send().unwrap();
 
     let mut body = String::new();
     res.read_to_string(&mut body).unwrap();
-
-    return  json::decode::<PlaylistItemResponse>(&body)
+    json::decode::<PlaylistItemResponse>(&body)
 }
 
+pub fn fetch_video(id: &str) -> json::DecodeResult<Video> {
+    let params = format!("key={}&part=snippet&id={}", *API_KEY, id);
+    let url    = format!("{}/{}?{}", BASE_URL, "videos", params);
+    let client = Client::new();
+    let mut res = client.get(&url)
+                        .header(Connection::close())
+                        .send().unwrap();
+    let mut body = String::new();
+    res.read_to_string(&mut body).unwrap();
+    let res = try!(json::decode::<VideoResponse>(&body));
+    if res.items.len() > 0 {
+        return Ok(res.items[0].clone());
+    }
+    Err(json::DecoderError::ApplicationError("track not found".to_string()))
+}
