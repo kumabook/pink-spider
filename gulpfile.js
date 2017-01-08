0547ebeb60dfedc512497c989a83de0520ba21f0
Hiroki Kumamoto <kumabook@live.jp>
2017-01-08 21:26:04 +0900

    Refactor gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-const gulp = require('gulp');
-const eslint = require('gulp-eslint');
+const gulp     = require('gulp');
+const eslint   = require('gulp-eslint');
 
-gulp.task('lint', () => {
-  return gulp.src(['web/js/**/*.js', 'web/js/**/*.jsx'])
-    .pipe(eslint({ fix: true}))
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
+gulp.task('lint', () =>
+          gulp.src(['web/js/**/*.{js,jsx}', 'web/test/**/*.{js,jsx}'])
+              .pipe(eslint({ fix: true}))
+              .pipe(eslint.format())
+              .pipe(eslint.failAfterError())
+         );
 
 gulp.task('default', ['lint'], function () {});
