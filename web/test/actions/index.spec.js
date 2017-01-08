fe8e04f85a1fb75717e74061a48df681cca9ffbb
Hiroki Kumamoto <kumabook@live.jp>
2017-01-08 21:41:41 +0900

    Add unit test for actions

---
+++ web/test/actions/index.spec.js
@@ -0,0 +1,73 @@
+import assert             from 'assert';
+import configureMockStore from 'redux-mock-store';
+import thunk              from 'redux-thunk';
+import axios              from 'axios';
+import MockAdapter        from 'axios-mock-adapter';
+import * as actions       from '../../js/actions';
+
+const mock = new MockAdapter(axios);
+axios.defaults.baseURL = 'http://0.0.0.0:8080';
+
+const middlewares = [thunk];
+const mockStore   = configureMockStore(middlewares);
+
+describe('actions', () => {
+  afterEach(() => {
+    mock.reset();
+  });
+  describe('.fetchEntries', () => {
+    it('creates RECEIVE_ENTRIES when fetching entries has been done', () => {
+      mock.onGet('/entries').reply(200, { items: [], page: 0, per_page: 10, total: 0 });
+      const expectedActions = [
+        { type: 'FETCH_ENTRIES', page: 0, perPage: 10 },
+        { type: 'RECEIVE_ENTRIES', page: 0, perPage: 10, total: 0, items: []},
+      ];
+      const store = mockStore();
+      return store.dispatch(actions.fetchEntries()).then(() => {
+        assert.deepEqual(store.getActions(), expectedActions);
+      });
+    });
+  });
+  describe('.fetchTracks', () => {
+    it('creates RECEIVE_TRACKS when fetching tracks has been done', () => {
+      mock.onGet('/tracks').reply(200, { items: [], page: 0, per_page: 10, total: 0 });
+      const expectedActions = [
+        { type: 'FETCH_TRACKS', entryId: null, page: 0, perPage: 10 },
+        { type: 'RECEIVE_TRACKS', entryId: null, page: 0, perPage: 10, total: 0, items: []},
+      ];
+      const store = mockStore();
+      return store.dispatch(actions.fetchTracks()).then(() => {
+        assert.deepEqual(store.getActions(), expectedActions);
+      });
+    });
+  });
+  describe('.fetchTrack', () => {
+    it('creates RECEIVE_TRACK when fetching a track has been done', () => {
+      const track = {
+        id: 'track_id',
+      };
+      mock.onGet('/tracks/track_id').reply(200, track);
+      const expectedActions = [
+        { type: 'FETCH_TRACK' },
+        { type: 'RECEIVE_TRACK', item: track },
+      ];
+      const store = mockStore();
+      return store.dispatch(actions.fetchTrack('track_id')).then(() => {
+        assert.deepEqual(store.getActions(), expectedActions);
+      });
+    });
+  });
+  describe('.updateTrack', () => {
+    it('creates UPDATE_TRACK when fetching a track has been done', () => {
+      const track = {
+        id: 'track_id',
+      };
+      mock.onPost('/tracks/track_id').reply(200, track);
+      const expectedActions = [{ type: 'UPDATE_TRACK' }];
+      const store = mockStore();
+      return store.dispatch(actions.updateTrack('track_id')).then(() => {
+        assert.deepEqual(store.getActions(), expectedActions);
+      });
+    });
+  });
+});
