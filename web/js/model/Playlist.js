76dd044017912de97b901cb79f516f973b4e52c6
Hiroki Kumamoto <kumabook@live.jp>
2017-02-22 23:56:43 +0900

    Add model/Playlist.js

---
+++ web/js/model/Playlist.js
@@ -0,0 +1,34 @@
+import url from 'url';
+import {
+  formatOpenURL,
+  parse,
+} from 'spotify-uri';
+
+export const getCountryParam = (urlString) => {
+  const parsed = url.parse(urlString);
+  if (parsed.query && parsed.query.country) {
+    return parsed.query.country;
+  }
+  return 'us';
+};
+
+export const getUrl = (playlist) => {
+  if (!playlist || !playlist.identifier) {
+    return null;
+  }
+  const id = playlist.identifier;
+  switch (playlist.provider) {
+    case 'YouTube':
+      return `https://www.youtube.com/watch/?v=${id}`;
+    case 'SoundCloud':
+      return `https://soundcloud.com/tracks/${id}`;
+    case 'Spotify':
+      return formatOpenURL(parse(playlist.url));
+    case 'AppleMusic': {
+      const country = getCountryParam(playlist.url);
+      return `http://tools.applemusic.com/embed/v1/playlist/pl.${id}?country=${country}`;
+    }
+    default:
+      return playlist.url;
+  }
+};
