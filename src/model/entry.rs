cef8bd59663c77f9d5030791c3f8fc75ff3b394b
Hiroki Kumamoto <kumabook@live.jp>
2017-02-26 17:49:06 +0900

    Entry conforms Model trait

--- src/model/entry.rs
+++ src/model/entry.rs
@@ -4,9 +4,11 @@ use postgres;
 use uuid::Uuid;
 use error::Error;
 use chrono::{NaiveDateTime, UTC, DateTime};
-use super::{conn, PaginatedCollection};
+use super::{conn, Model};
+use model::enclosure::Enclosure;
 use Track;
 use Playlist;
+use Album;
 
 static PROPS: [&'static str; 8]  = ["id",
                                     "url",
@@ -16,10 +18,6 @@ static PROPS: [&'static str; 8]  = ["id",
                                     "locale",
                                     "created_at",
                                     "updated_at"];
-
-fn props_str() -> String {
-    PROPS.join(",")
-}
 
 #[derive(Debug, Clone)]
 pub struct Entry {
@@ -33,6 +31,7 @@ pub struct Entry {
     pub updated_at:  NaiveDateTime,
     pub tracks:      Vec<Track>,
     pub playlists:   Vec<Playlist>,
+    pub albums:      Vec<Album>,
 }
 
 impl ToJson for Entry {
@@ -58,8 +57,17 @@ impl ToJson for Entry {
     }
 }
 
-impl Entry {
-    fn rows_to_entries(rows: postgres::rows::Rows) -> Vec<Entry> {
+impl Model for Entry {
+    fn table_name() -> String {
+        "entries".to_string()
+    }
+    fn props_str(prefix: &str) -> String {
+        PROPS
+            .iter()
+            .map(|&p| format!("{}{}", prefix, p))
+            .collect::<Vec<String>>().join(",")
+    }
+    fn rows_to_items(rows: postgres::rows::Rows) -> Vec<Entry> {
         let mut entries = Vec::new();
         for row in rows.iter() {
             entries.push(Entry {
@@ -73,69 +81,52 @@ impl Entry {
                 updated_at:  row.get(7),
                 tracks:      Track::find_by_entry_id(row.get(0)),
                 playlists:   Playlist::find_by_entry_id(row.get(0)),
+                albums:      Album::find_by_entry_id(row.get(0)),
             })
         }
         entries
     }
-
-    pub fn find_by_id(id: String) -> Result<Entry, Error> {
-        let conn = conn().unwrap();
-        let stmt = conn.prepare(
-            &format!("SELECT {} FROM entries
-                        WHERE id = $1", props_str())).unwrap();
-        let rows = stmt.query(&[&id]).unwrap();
-        let entries = Entry::rows_to_entries(rows);
-        if entries.len() > 0 {
-            return Ok(entries[0].clone());
+    fn create(&self) -> Result<Entry, Error> {
+        let conn = try!(conn());
+        let stmt = try!(conn.prepare("INSERT INTO entries (url) VALUES ($1) RETURNING id"));
+        let rows = try!(stmt.query(&[&self.url]));
+        let mut entry = self.clone();
+        for row in rows.iter() {
+            entry.id = row.get(0);
         }
-        return Err(Error::NotFound)
+        Ok(entry)
     }
-
-    pub fn find_by_url(url: &str) -> Result<Entry, Error> {
-        let conn = conn().unwrap();
-        let stmt = conn.prepare(
-            &format!("SELECT {} FROM entries
-                        WHERE url = $1", props_str())).unwrap();
-        let rows = stmt.query(&[&url]).unwrap();
-        let entries = Entry::rows_to_entries(rows);
-        if entries.len() > 0 {
-            return Ok(entries[0].clone());
-        }
-        return Err(Error::NotFound)
+    fn save(&mut self) -> Result<(), Error> {
+        self.updated_at = UTC::now().naive_utc();
+        let conn = try!(conn());
+        let stmt = try!(conn.prepare("UPDATE entries SET
+                                   url         = $2,
+                                   title       = $3,
+                                   description = $4,
+                                   visual_url  = $5,
+                                   locale      = $6,
+                                   created_at  = $7,
+                                   updated_at  = $8
+                                 WHERE id = $1"));
+        let result = stmt.query(&[&self.id,
+                                  &self.url,
+                                  &self.title,
+                                  &self.description,
+                                  &self.visual_url,
+                                  &self.locale,
+                                  &self.created_at,
+                                  &self.updated_at]);
+        try!(result);
+        Ok(())
     }
+}
 
-    pub fn find(page: i64, per_page: i64) -> PaginatedCollection<Entry> {
-        let conn = conn().unwrap();
-        let stmt = conn.prepare(
-            &format!("SELECT {} FROM entries
-                        ORDER BY updated_at DESC
-                        LIMIT $2 OFFSET $1", props_str())).unwrap();
-        let offset = page * per_page;
-        let rows = stmt.query(&[&offset, &per_page]).unwrap();
-        let entries = Entry::rows_to_entries(rows);
-        let mut total: i64 = 0;
-        for row in conn.query("SELECT COUNT(*) FROM entries", &[]).unwrap().iter() {
-            total = row.get(0);
-        }
-        PaginatedCollection {
-            page:     page,
-            per_page: per_page,
-            total:    total,
-            items:    entries,
-        }
-    }
-
-    pub fn find_or_create_by_url(url: String) -> Result<Entry, Error> {
-        match Entry::find_by_url(&url) {
-            Ok(entry) => Ok(entry),
-            Err(_)    => Entry::create_by_url(url)
-        }
-    }
-
-    pub fn create_by_url(url: String) -> Result<Entry, Error> {
-        let conn = conn().unwrap();
-        let stmt = conn.prepare("INSERT INTO entries (url) VALUES ($1) RETURNING id").unwrap();
-        for row in stmt.query(&[&url]).unwrap().iter() {
+impl Entry {
+    pub fn new(url: String) -> Result<Entry, Error> {
+        let conn = try!(conn());
+        let stmt = try!(conn.prepare("INSERT INTO entries (url) VALUES ($1) RETURNING id"));
+        let rows = try!(stmt.query(&[&url]));
+        for row in rows.iter() {
             let entry = Entry {
                 id:          row.get(0),
                 url:         url,
@@ -147,49 +138,80 @@ impl Entry {
                 updated_at:  UTC::now().naive_utc(),
                 tracks:      Vec::new(),
                 playlists:   Vec::new(),
+                albums:      Vec::new(),
             };
             return Ok(entry);
         }
         Err(Error::Unexpected)
     }
 
-    pub fn add_track(&mut self, track: Track) {
-        let conn = conn().unwrap();
-        let stmt = conn.prepare("INSERT INTO track_entries (track_id, entry_id)
-                                 VALUES ($1, $2)").unwrap();
-        stmt.query(&[&track.id, &self.id]).unwrap();
+    pub fn find_by_url(url: &str) -> Result<Entry, Error> {
+        let conn = try!(conn());
+        let stmt = try!(conn.prepare(
+            &format!("SELECT {} FROM entries
+                        WHERE url = $1", Self::props_str(""))));
+        let rows = try!(stmt.query(&[&url]));
+        let entries = Entry::rows_to_items(rows);
+        if entries.len() > 0 {
+            return Ok(entries[0].clone());
+        }
+        return Err(Error::NotFound)
+    }
+
+    pub fn find_or_create_by_url(url: String) -> Result<Entry, Error> {
+        match Entry::find_by_url(&url) {
+            Ok(entry) => Ok(entry),
+            Err(_)    => Entry::create_by_url(url)
+        }
+    }
+
+    pub fn create_by_url(url: String) -> Result<Entry, Error> {
+        let conn = try!(conn());
+        let stmt = try!(conn.prepare("INSERT INTO entries (url) VALUES ($1) RETURNING id"));
+        let rows = try!(stmt.query(&[&url]));
+        for row in rows.iter() {
+            let entry = Entry {
+                id:          row.get(0),
+                url:         url,
+                title:       None,
+                description: None,
+                visual_url:  None,
+                locale:      None,
+                created_at:  UTC::now().naive_utc(),
+                updated_at:  UTC::now().naive_utc(),
+                tracks:      Vec::new(),
+                playlists:   Vec::new(),
+                albums:      Vec::new(),
+            };
+            return Ok(entry);
+        }
+        Err(Error::Unexpected)
+    }
+
+    pub fn add_track(&mut self, track: Track) -> Result<(), Error> {
+        let conn = try!(conn());
+        let stmt = try!(conn.prepare("INSERT INTO track_entries (track_id, entry_id)
+                                 VALUES ($1, $2)"));
+        try!(stmt.query(&[&track.id, &self.id]));
         self.tracks.push(track);
+        Ok(())
     }
 
-    pub fn add_playlist(&mut self, playlist: Playlist) {
-        let conn = conn().unwrap();
-        let stmt = conn.prepare("INSERT INTO playlist_entries (playlist_id, entry_id)
-                                 VALUES ($1, $2)").unwrap();
-        stmt.query(&[&playlist.id, &self.id]).unwrap();
+    pub fn add_playlist(&mut self, playlist: Playlist) -> Result<(), Error> {
+        let conn = try!(conn());
+        let stmt = try!(conn.prepare("INSERT INTO playlist_entries (playlist_id, entry_id)
+                                 VALUES ($1, $2)"));
+        try!(stmt.query(&[&playlist.id, &self.id]));
         self.playlists.push(playlist);
+        Ok(())
     }
 
-    pub fn save(&mut self) -> Result<(), Error> {
-        self.updated_at = UTC::now().naive_utc();
-        let conn = conn().unwrap();
-        let stmt = conn.prepare("UPDATE entries SET
-                                   url         = $2,
-                                   title       = $3,
-                                   description = $4,
-                                   visual_url  = $5,
-                                   locale      = $6,
-                                   created_at  = $7,
-                                   updated_at  = $8
-                                 WHERE id = $1").unwrap();
-        let result = stmt.query(&[&self.id,
-                                  &self.url,
-                                  &self.title,
-                                  &self.description,
-                                  &self.visual_url,
-                                  &self.locale,
-                                  &self.created_at,
-                                  &self.updated_at]);
-        try!(result);
+    pub fn add_album(&mut self, album: Album) -> Result<(), Error> {
+        let conn = try!(conn());
+        let stmt = try!(conn.prepare("INSERT INTO album_entries (album_id, entry_id)
+                                 VALUES ($1, $2)"));
+        try!(stmt.query(&[&album.id, &self.id]));
+        self.albums.push(album);
         Ok(())
     }
 }
