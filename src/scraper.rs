07bedad443ed6a2dd7570ce5db2d8b879ae29061
Hiroki Kumamoto <kumabook@live.jp>
2017-05-09 17:01:02 +0900

    Update scraper unit test

--- src/scraper.rs
+++ src/scraper.rs
@@ -401,7 +401,7 @@ mod test {
         let product   = extract(url).unwrap();
         let playlists = product.playlists;
         let tracks    = product.tracks;
-        assert_eq!(playlists.len(), 2);
+        assert_eq!(playlists.len(), 3);
         let youtube_tracks: Vec<&Track> = tracks.iter().filter(|&x| x.provider == Provider::YouTube).collect();
         let spotify_tracks: Vec<&Track> = tracks.iter().filter(|&x| x.provider == Provider::Spotify).collect();
         assert_eq!(youtube_tracks.len(), 15);
