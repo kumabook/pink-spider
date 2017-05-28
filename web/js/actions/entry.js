6b64347f265934ad6250f6887c11b93b32f3d144
Hiroki Kumamoto <kumabook@live.jp>
2017-05-28 17:58:39 +0900

    Implement actions of resources

---
+++ web/js/actions/entry.js
@@ -0,0 +1,14 @@
+import { createResourceActions } from 'material-jsonschema';
+import { schema }                from '../model/Entry';
+
+const actions = createResourceActions(schema);
+
+export default actions;
+
+export const index   = actions.index;
+export const show    = actions.show;
+export const create  = actions.create;
+export const update  = actions.update;
+export const destroy = actions.destroy;
+
+export const creators = actions.creators;
