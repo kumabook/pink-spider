857b2628d5dfda61a3fd56933e38df40eb23f1c1
Hiroki Kumamoto <kumabook@live.jp>
2017-05-28 18:12:54 +0900

    Use material-jsonschema Table for PlaylistList

--- web/js/containers/PlaylistList.jsx
+++ web/js/containers/PlaylistList.jsx
@@ -1,116 +1,94 @@
-import React             from 'react';
-import PropTypes         from 'prop-types';
-import { connect }       from 'react-redux';
-import { withRouter }    from 'react-router-dom';
-import { push, replace } from 'react-router-redux';
+import React              from 'react';
+import PropTypes          from 'prop-types';
+import { connect }        from 'react-redux';
+import { withRouter }     from 'react-router-dom';
+import { push }           from 'react-router-redux';
+import { Table }          from 'material-jsonschema';
+import parseIntOr         from '../utils/parseIntOr';
+import { creators }       from '../actions/playlist';
+import { defaultPerPage } from '../config';
 import {
-  Table,
-  TableBody,
-  TableHeader,
-  TableHeaderColumn,
-  TableRow,
-} from 'material-ui/Table';
-import Dialog               from 'material-ui/Dialog';
-import CircularProgress     from 'material-ui/CircularProgress';
-import { fetchPlaylists }   from '../actions';
-import { Status }           from '../reducers/playlists';
-import parseIntOr           from '../utils/parseIntOr';
-import { DEFAULT_PER_PAGE } from '../api/pagination';
-import PlaylistListTableRow from '../components/PlaylistListTableRow';
-import Paginate             from '../components/Paginate';
+  schema,
+  tableSchema,
+  formSchema,
+} from '../model/Playlist';
+
+const { update } = creators;
 
 class PlaylistList extends React.Component {
   static get propTypes() {
     return {
-      playlists:               PropTypes.object.isRequired,
-      page:                    PropTypes.number.isRequired,
-      fetchPlaylists:          PropTypes.func.isRequired,
-      handleDetailButtonClick: PropTypes.func.isRequired,
-      handlePageChange:        PropTypes.func.isRequired,
+      items:   PropTypes.array.isRequired,
+      total:   PropTypes.number.isRequired,
+      page:    PropTypes.number.isRequired,
+      perPage: PropTypes.number.isRequired,
+      index:   PropTypes.func.isRequired,
+      show:    PropTypes.func.isRequired,
+      update:  PropTypes.func.isRequired,
     };
   }
-  componentDidUpdate() {
-    if (this.props.playlists.status === Status.Dirty) {
-      this.props.fetchPlaylists(this.props.playlists.page,
-                                this.props.playlists.perPage);
+  handleAction(name, item) {
+    switch (name) {
+      case 'detail':
+        this.props.show(item);
+        break;
+      case 'reload':
+        this.props.update(item);
+        break;
+      default:
+        break;
     }
   }
   render() {
-    const rows = this.props.playlists.items.map(playlist => (
-      <PlaylistListTableRow
-        key={playlist.id}
-        playlist={playlist}
-        onDetailButtonClick={this.props.handleDetailButtonClick}
-      />
-    ));
     return (
-      <div>
-        <Table selectable={false}>
-          <TableHeader displaySelectAll={false}>
-            <TableRow>
-              <TableHeaderColumn colSpan="4" style={{ textAlign: 'center' }}>
-                <Paginate
-                  page={this.props.page}
-                  pageCount={this.props.playlists.total / this.props.playlists.perPage}
-                  onChange={this.props.handlePageChange}
-                />
-              </TableHeaderColumn>
-            </TableRow>
-            <TableRow>
-              <TableHeaderColumn>thumbnail</TableHeaderColumn>
-              <TableHeaderColumn>title</TableHeaderColumn>
-              <TableHeaderColumn>owner</TableHeaderColumn>
-              <TableHeaderColumn>published at</TableHeaderColumn>
-              <TableHeaderColumn>buttons</TableHeaderColumn>
-            </TableRow>
-          </TableHeader>
-          <TableBody displayRowCheckbox={false}>
-            {rows}
-          </TableBody>
-        </Table>
-        <Dialog
-          modal
-          title="Loading..."
-          titleStyle={{ textAlign: 'center' }}
-          bodyStyle={{ textAlign: 'center' }}
-          open={this.props.playlists.status === Status.Dirty}
-        >
-          <CircularProgress mode="indeterminate" />
-        </Dialog>
-      </div>
+      <Table
+        schema={schema}
+        tableSchema={tableSchema}
+        formSchema={formSchema}
+        items={this.props.items}
+        page={this.props.page}
+        perPage={this.props.perPage}
+        pageCount={this.props.total / 10}
+        onPageChange={this.props.index}
+        canCreate={false}
+        canEdit={false}
+        canDestroy={false}
+        onAction={(name, item) => this.handleAction(name, item)}
+      />
     );
   }
 }
 
-function mapStateToProps(state, { location, match }) {
-  const query = new URLSearchParams(location.search);
+function mapStateToProps(state) {
+  const query   = new URLSearchParams(state.router.location.search);
+  const page    = parseIntOr(query.get('page'), 0);
+  const perPage = parseIntOr(query.get('per_page'), defaultPerPage);
   return {
-    playlists: state.playlists,
-    page:      parseIntOr(query.get('page'), 0),
-    entry_id:  match.params.entry_id,
+    item:  state.playlists.item,
+    items: state.playlists.items,
+    total: state.playlists.total,
+    page,
+    perPage,
   };
 }
 
-function mapDispatchToProps(dispatch, { location, match }) {
-  const query = new URLSearchParams(location.search);
-  const page = parseIntOr(query.get('page'), 0);
-  const perPage = parseIntOr(query.get('per_page'), DEFAULT_PER_PAGE);
-  const entryId = match.params.entry_id;
+function mapDispatchToProps(dispatch, { match: { params } }) {
   return {
-    fetchPlaylists:          () => dispatch(fetchPlaylists(page, undefined, entryId)),
-    handleDetailButtonClick: playlist => dispatch(push({ pathname: `playlists/${playlist.id}` })),
-    handlePageChange:        (data) => {
-      const path = entryId ? `entries/${entryId}/playlists` : 'playlists';
-      const params = new URLSearchParams();
-      params.append('page', data.selected);
-      params.append('per_page', perPage);
-      const loc = { pathname: path, search: params.toString() };
-      if (parseIntOr(query.get('page'), 0) === data.selected) {
-        dispatch(replace(loc));
-      } else {
-        dispatch(push(loc));
+    index: (page, perPage) => {
+      const searchParams = new URLSearchParams();
+      searchParams.append('page', page);
+      searchParams.append('per_page', perPage);
+      let path = '/playlists';
+      if (params.entry_id) {
+        path = `/entries/${params.entry_id}/playlists`;
       }
+      dispatch(push({
+        pathname: path,
+        search:   params.toString(),
+      }));
     },
+    show:   ({ id }) => dispatch(push({ pathname: `/playlists/${id}` })),
+    update: item => dispatch(update.start(item)),
   };
 }
 
