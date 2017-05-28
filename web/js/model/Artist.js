a24cd32380244943add7cbcfd056b36479ac486d
Hiroki Kumamoto <kumabook@live.jp>
2017-05-28 18:09:24 +0900

    Add json schema of Artist

--- web/js/model/Artist.js
+++ web/js/model/Artist.js
@@ -1,8 +1,42 @@
+import ActionUpdate from 'material-ui/svg-icons/action/update';
 import {
   formatOpenURL,
   parse,
 } from 'spotify-uri';
 import { getCountryParam } from './Playlist';
+
+export const schema = {
+  title: 'Artist',
+  type:  'object',
+
+  properties: {
+    id:            { type: 'string' },
+    provider:      { type: 'string' },
+    identifier:    { type: 'string' },
+    url:           { type: 'string' },
+    name:          { type: 'string' },
+    thumbnail_url: { type: 'string', format: 'data-url' },
+    artwork_url:   { type: 'string', format: 'data-url' },
+  },
+  required: [],
+};
+
+export const tableSchema = {
+  'ui:order':   ['thumbnail_url', 'name'],
+  'ui:actions': [
+    { name: 'reload', icon: ActionUpdate },
+  ],
+  id:            { 'ui:widget': 'hidden' },
+  provider:      { 'ui:widget': 'hidden' },
+  identifier:    { 'ui:widget': 'hidden' },
+  url:           { 'ui:widget': 'hidden' },
+  name:          {},
+  thumbnail_url: { 'ui:widget': 'img' },
+  artwork_url:   { 'ui:widget': 'hidden' },
+};
+
+export const formSchema = {
+};
 
 export const dummu = '';
 
