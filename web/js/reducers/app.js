c990baf6310a871e2d2e3839bdde526ffb369ee9
Hiroki Kumamoto <kumabook@live.jp>
2017-05-28 18:02:42 +0900

    Update web/js/reducers/app.js

--- web/js/reducers/app.js
+++ web/js/reducers/app.js
@@ -1,8 +1,39 @@
-const initialState = { drawlerIsOpen: false };
+import { combineReducers } from 'redux';
 
-export default function toggleDrawler(state = initialState, action) {
+const drawlerIsOpen = (state = false, action) => {
   if (action.type === 'TOGGLE_DRAWLER') {
-    return { drawlerIsOpen: !state.drawlerIsOpen };
+    return !state;
   }
   return state;
-}
+};
+
+const progress = (state = false, action) => {
+  switch (action.type) {
+    case 'SHOW_PROGRESS':
+      return true;
+    case 'HIDE_PROGRESS':
+      return false;
+    default:
+      return state;
+  }
+};
+
+const message = (state = '', action) => {
+  switch (action.type) {
+    case 'SHOW_MESSAGE':
+      return action.payload;
+    case 'CLOSE_MESSAGE':
+      return '';
+    default:
+      return state;
+  }
+};
+
+const titleReducer = (state = '') => state;
+
+export default combineReducers({
+  drawlerIsOpen,
+  message,
+  progress,
+  title: titleReducer,
+});
