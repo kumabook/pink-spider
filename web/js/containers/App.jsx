3b238c72cfc68cba3fbd9509900b189f84ac2828
Hiroki Kumamoto <kumabook@live.jp>
2017-05-28 18:07:54 +0900

    App component showes progress during api request

--- web/js/containers/App.jsx
+++ web/js/containers/App.jsx
@@ -3,10 +3,21 @@ import PropTypes         from 'prop-types';
 import AppBar            from 'material-ui/AppBar';
 import Drawer            from 'material-ui/Drawer';
 import MenuItem          from 'material-ui/MenuItem';
+import CircularProgress  from 'material-ui/CircularProgress';
 import { connect }       from 'react-redux';
 import { push }          from 'react-router-redux';
 import { withRouter }    from 'react-router-dom';
 import { toggleDrawler } from '../actions';
+
+const progressStyle = {
+  position:        'absolute',
+  top:             0,
+  left:            0,
+  width:           '100%',
+  height:          '100%',
+  backgroundColor: 'rgba(10, 10, 10, 0.4)',
+  zIndex:          2000,
+};
 
 class App extends React.Component {
   static get propTypes() {
@@ -19,9 +30,15 @@ class App extends React.Component {
       handleTracksMenuClick:    PropTypes.func.isRequired,
       handleArtistsMenuClick:   PropTypes.func.isRequired,
       drawlerIsOpen:            PropTypes.bool.isRequired,
+      progress:                 PropTypes.bool.isRequired,
     };
   }
   render() {
+    const progress = !this.props.progress ? null : (
+      <div style={progressStyle}>
+        <CircularProgress style={{ marginTop: '45%', marginLeft: '49%' }} />
+      </div>
+    );
     return (
       <div>
         <AppBar
@@ -41,6 +58,7 @@ class App extends React.Component {
           <MenuItem onTouchTap={this.props.handleArtistsMenuClick}>Artists</MenuItem>
         </Drawer>
         {this.props.children}
+        {progress}
       </div>
     );
   }
@@ -48,7 +66,10 @@ class App extends React.Component {
 
 function mapStateToProps(state) {
   return {
+    title:         state.app.title,
     drawlerIsOpen: state.app.drawlerIsOpen,
+    message:       state.app.message,
+    progress:      state.app.progress,
   };
 }
 
