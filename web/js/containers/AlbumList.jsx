c69465087692901ca8f170a6fac4e798ed408eeb
Hiroki Kumamoto <kumabook@live.jp>
2017-05-28 18:12:18 +0900

    Use material-jsonschema Table for AlbumList

--- web/js/containers/AlbumList.jsx
+++ web/js/containers/AlbumList.jsx
@@ -1,118 +1,94 @@
-import React             from 'react';
-import PropTypes         from 'prop-types';
-import { connect }       from 'react-redux';
-import { withRouter }    from 'react-router-dom';
-import { push, replace } from 'react-router-redux';
+import React              from 'react';
+import PropTypes          from 'prop-types';
+import { connect }        from 'react-redux';
+import { withRouter }     from 'react-router-dom';
+import { push }           from 'react-router-redux';
+import { Table }          from 'material-jsonschema';
+import parseIntOr         from '../utils/parseIntOr';
+import { creators }       from '../actions/album';
+import { defaultPerPage } from '../config';
 import {
-  Table,
-  TableBody,
-  TableHeader,
-  TableHeaderColumn,
-  TableRow,
-} from 'material-ui/Table';
-import Dialog                       from 'material-ui/Dialog';
-import CircularProgress             from 'material-ui/CircularProgress';
-import { fetchAlbums }              from '../actions';
-import { Status }                   from '../reducers/albums';
-import parseIntOr                   from '../utils/parseIntOr';
-import { DEFAULT_PER_PAGE }         from '../api/pagination';
-import AlbumListTableRow            from '../components/AlbumListTableRow';
-import Paginate                     from '../components/Paginate';
+  schema,
+  tableSchema,
+  formSchema,
+} from '../model/Album';
+
+const { update } = creators;
 
 class AlbumList extends React.Component {
   static get propTypes() {
     return {
-      albums:                  PropTypes.object.isRequired,
-      page:                    PropTypes.number.isRequired,
-      fetchAlbums:             PropTypes.func.isRequired,
-      handleDetailButtonClick: PropTypes.func.isRequired,
-      handleUpdateButtonClick: PropTypes.func.isRequired,
-      handlePageChange:        PropTypes.func.isRequired,
+      items:   PropTypes.array.isRequired,
+      total:   PropTypes.number.isRequired,
+      page:    PropTypes.number.isRequired,
+      perPage: PropTypes.number.isRequired,
+      index:   PropTypes.func.isRequired,
+      update:  PropTypes.func.isRequired,
+      show:    PropTypes.func.isRequired,
     };
   }
-  componentDidUpdate() {
-    if (this.props.albums.status === Status.Dirty) {
-      this.props.fetchAlbums(this.props.albums.page,
-                             this.props.albums.perPage);
+  handleAction(name, item) {
+    switch (name) {
+      case 'detail':
+        this.props.show(item);
+        break;
+      case 'reload':
+        this.props.update(item);
+        break;
+      default:
+        break;
     }
   }
   render() {
-    const rows = this.props.albums.items.map(album => (
-      <AlbumListTableRow
-        key={album.id}
-        album={album}
-        onDetailButtonClick={this.props.handleDetailButtonClick}
-        onUpdateButtonClick={this.props.handleUpdateButtonClick}
-      />
-    ));
     return (
-      <div>
-        <Table selectable={false}>
-          <TableHeader displaySelectAll={false}>
-            <TableRow>
-              <TableHeaderColumn colSpan="5" style={{ textAlign: 'center' }}>
-                <Paginate
-                  page={this.props.page}
-                  pageCount={this.props.albums.total / this.props.albums.perPage}
-                  onChange={this.props.handlePageChange}
-                />
-              </TableHeaderColumn>
-            </TableRow>
-            <TableRow>
-              <TableHeaderColumn>thumbnail</TableHeaderColumn>
-              <TableHeaderColumn>title</TableHeaderColumn>
-              <TableHeaderColumn>artist</TableHeaderColumn>
-              <TableHeaderColumn>published at</TableHeaderColumn>
-              <TableHeaderColumn>buttons</TableHeaderColumn>
-            </TableRow>
-          </TableHeader>
-          <TableBody displayRowCheckbox={false}>
-            {rows}
-          </TableBody>
-        </Table>
-        <Dialog
-          modal
-          title="Loading..."
-          titleStyle={{ textAlign: 'center' }}
-          bodyStyle={{ textAlign: 'center' }}
-          open={this.props.albums.status === Status.Dirty}
-        >
-          <CircularProgress mode="indeterminate" />
-        </Dialog>
-      </div>
+      <Table
+        schema={schema}
+        tableSchema={tableSchema}
+        formSchema={formSchema}
+        items={this.props.items}
+        page={this.props.page}
+        perPage={this.props.perPage}
+        pageCount={this.props.total / 10}
+        onPageChange={this.props.index}
+        canCreate={false}
+        canEdit={false}
+        canDestroy={false}
+        onAction={(name, item) => this.handleAction(name, item)}
+      />
     );
   }
 }
 
-function mapStateToProps(state, { location, match }) {
-  const query = new URLSearchParams(location.search);
+function mapStateToProps(state) {
+  const query   = new URLSearchParams(state.router.location.search);
+  const page    = parseIntOr(query.get('page'), 0);
+  const perPage = parseIntOr(query.get('per_page'), defaultPerPage);
   return {
-    albums:   state.albums,
-    page:     parseIntOr(query.get('page'), 0),
-    entry_id: match.params.entry_id,
+    item:  state.albums.item,
+    items: state.albums.items,
+    total: state.albums.total,
+    page,
+    perPage,
   };
 }
 
-function mapDispatchToProps(dispatch, { location, match }) {
-  const query = new URLSearchParams(location.search);
-  const page = parseIntOr(query.get('page'), 0);
-  const perPage = parseIntOr(query.get('per_page'), DEFAULT_PER_PAGE);
-  const entryId = match.params.entry_id;
+function mapDispatchToProps(dispatch, { match: { params } }) {
   return {
-    fetchAlbums:             () => dispatch(fetchAlbums(page, undefined, entryId)),
-    handleDetailButtonClick: album => dispatch(push({ pathname: `albums/${album.id}` })),
-    handlePageChange:        (data) => {
-      const path = entryId ? `entries/${entryId}/albums` : 'albums';
-      const params = new URLSearchParams();
-      params.append('page', data.selected);
-      params.append('per_page', perPage);
-      const loc = { pathname: path, search: params.toString() };
-      if (parseIntOr(query.get('page'), 0) === data.seltected) {
-        dispatch(replace(loc));
-      } else {
-        dispatch(push(loc));
+    index: (page, perPage) => {
+      const searchParams = new URLSearchParams();
+      searchParams.append('page', page);
+      searchParams.append('per_page', perPage);
+      let path = '/albums';
+      if (params.entry_id) {
+        path = `/entries/${params.entry_id}/albums`;
       }
+      dispatch(push({
+        pathname: path,
+        search:   searchParams.toString(),
+      }));
     },
+    show:   ({ id }) => dispatch(push({ pathname: `/albums/${id}` })),
+    update: item => dispatch(update.start(item)),
   };
 }
 
