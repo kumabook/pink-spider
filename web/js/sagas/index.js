3ac5a1d326288cb8fa3532bfa1feabd92ff504e6
Hiroki Kumamoto <kumabook@live.jp>
2017-05-28 17:20:54 +0900

    Add sagas/index.js

---
+++ web/js/sagas/index.js
@@ -0,0 +1,50 @@
+import {
+  fork,
+} from 'redux-saga/effects';
+import {
+  watchFetchEntries,
+  watchFetchEntry,
+  watchUpdateEntry,
+} from './entry';
+import {
+  watchFetchTracks,
+  watchFetchTrack,
+  watchUpdateTrack,
+} from './track';
+import {
+  watchFetchAlbums,
+  watchFetchAlbum,
+  watchUpdateAlbum,
+} from './album';
+import {
+  watchFetchPlaylists,
+  watchFetchPlaylist,
+  watchUpdatePlaylist,
+} from './playlist';
+import {
+  watchFetchArtists,
+  watchFetchArtist,
+  watchUpdateArtist,
+} from './artist';
+import routerSaga from './router';
+
+export default function* root() {
+  yield [
+    fork(watchFetchEntries),
+    fork(watchFetchEntry),
+    fork(watchUpdateEntry),
+    fork(watchFetchTracks),
+    fork(watchFetchTrack),
+    fork(watchUpdateTrack),
+    fork(watchFetchAlbums),
+    fork(watchFetchAlbum),
+    fork(watchUpdateAlbum),
+    fork(watchFetchPlaylists),
+    fork(watchFetchPlaylist),
+    fork(watchUpdatePlaylist),
+    fork(watchFetchArtists),
+    fork(watchFetchArtist),
+    fork(watchUpdateArtist),
+    fork(routerSaga),
+  ];
+}
