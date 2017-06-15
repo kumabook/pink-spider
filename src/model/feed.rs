c0f3bf9581385d9f81bee13b346ad629a0e3fd56
Hiroki Kumamoto <kumabook@live.jp>
2017-06-15 15:06:43 +0900

    Implement Feed struct

--- src/model/feed.rs
+++ src/model/feed.rs
@@ -1,98 +1,237 @@
+use postgres;
+use uuid::Uuid;
 use error::Error;
-use super::conn;
+use chrono::{NaiveDateTime, UTC};
+use super::{conn, Model, Entry};
+use model::state::State;
+use rss;
+
+static PROPS: [&'static str; 15]  = ["id",
+                                     "url",
+                                     "title",
+                                     "description",
+                                     "language",
+                                     "velocity",
+                                     "website",
+                                     "state",
+                                     "last_updated",
+                                     "crawled",
+                                     "visual_url",
+                                     "icon_url",
+                                     "cover_url",
+                                     "created_at",
+                                     "updated_at"];
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Feed {
-    pub id:           String,
-    pub subscribers:  Option<i64>,
-    pub title:        Option<String>,
+    pub id:           Uuid,
+    pub url:          String,
+    pub title:        String,
     pub description:  Option<String>,
     pub language:     Option<String>,
-    pub velocity:     Option<f64>,
+    pub velocity:     f64,
     pub website:      Option<String>,
-    pub topics:       Option<Vec<String>>,
-    pub status:       Option<String>,
-    pub curated:      Option<bool>,
-    pub featured:     Option<bool>,
-    pub last_updated: Option<i64>,
+    pub state:        State,
+    pub last_updated: NaiveDateTime,
+    pub crawled:      NaiveDateTime,
 
     pub visual_url:   Option<String>,
     pub icon_url:     Option<String>,
     pub cover_url:    Option<String>,
+
+    pub created_at:   NaiveDateTime,
+    pub updated_at:   NaiveDateTime,
+}
+
+impl<'a> Model<'a> for Feed {
+    fn table_name() -> String {
+        "feeds".to_string()
+    }
+    fn props_str(prefix: &str) -> String {
+        PROPS
+            .iter()
+            .map(|&p| format!("{}{}", prefix, p))
+            .collect::<Vec<String>>().join(",")
+    }
+    fn rows_to_items(rows: postgres::rows::Rows) -> Vec<Feed> {
+        let mut feeds = Vec::new();
+        for row in rows.iter() {
+            feeds.push(Feed {
+                id:           row.get(0),
+                url:          row.get(1),
+                title:        row.get(2),
+                description:  row.get(3),
+                language:     row.get(4),
+                velocity:     row.get(5),
+                website:      row.get(6),
+                state:        State::new(row.get(7)),
+                last_updated: row.get(8),
+                crawled:      row.get(9),
+
+                visual_url:   row.get(10),
+                icon_url:     row.get(11),
+                cover_url:    row.get(12),
+
+                created_at:   row.get(13),
+                updated_at:   row.get(14),
+            })
+        }
+        feeds
+    }
+    fn create(&self) -> Result<Feed, Error> {
+        let conn = try!(conn());
+        let stmt = try!(conn.prepare("INSERT INTO feeds (id, u) VALUES ($1)"));
+        let rows = try!(stmt.query(&[
+            &self.id,
+            &self.url,
+            &self.title,
+            &self.description,
+            &self.language,
+            &self.velocity,
+            &self.website,
+            &self.state.to_string(),
+            &self.last_updated,
+            &self.crawled,
+            &self.visual_url,
+            &self.icon_url,
+            &self.cover_url,
+            &self.created_at,
+            &self.updated_at,
+        ]));
+        let mut feed = self.clone();
+        for row in rows.iter() {
+            feed.id = row.get(0);
+        }
+        Ok(feed)
+    }
+    fn save(&mut self) -> Result<(), Error> {
+        self.updated_at = UTC::now().naive_utc();
+        let conn = try!(conn());
+        let stmt = try!(conn.prepare("UPDATE feeds SET
+                                   url          = $2,
+                                   title        = $3,
+                                   description  = $4,
+                                   language     = $5,
+                                   velocity     = $6,
+                                   website      = $7,
+                                   state        = $8,
+                                   last_updated = $9,
+                                   crawled      = $10,
+                                   visual_url   = $11,
+                                   icon_url     = $12,
+                                   cover_url    = $13,
+                                   created_at   = $14,
+                                   updated_at   = $15
+                                 WHERE id = $1"));
+        let result = stmt.query(&[&self.id,
+                                  &self.url,
+                                  &self.title,
+                                  &self.description,
+                                  &self.language,
+                                  &self.velocity,
+                                  &self.website,
+                                  &self.state.to_string(),
+                                  &self.last_updated,
+                                  &self.crawled,
+                                  &self.visual_url,
+                                  &self.icon_url,
+                                  &self.cover_url,
+                                  &self.created_at,
+                                  &self.updated_at]);
+        try!(result);
+        Ok(())
+    }
 }
 
 impl Feed {
-    pub fn find_by_id(id: String) -> Result<Feed, Error> {
-        let conn = conn().unwrap();
-        let stmt = conn.prepare("SELECT id, subscribers, title, description, language, velocity, website, status, curated, featured, last_updated, visual_url, icon_url, cover_url FROM feeds WHERE id = $1").unwrap();
-        for row in stmt.query(&[&id]).unwrap().iter() {
-            return Ok(Feed {
-                    id: row.get(0),
-           subscribers: row.get(1),
-                 title: row.get(2),
-           description: row.get(3),
-              language: row.get(4),
-              velocity: row.get(5),
-               website: row.get(6),
-                topics: None,
-                status: row.get(7),
-               curated: row.get(8),
-              featured: row.get(9),
-          last_updated: row.get(10),
-            visual_url: row.get(11),
-              icon_url: row.get(12),
-             cover_url: row.get(13),
-            });
+    pub fn find_by_url(url: &str) -> Result<Feed, Error> {
+        let conn = try!(conn());
+        let stmt = try!(conn.prepare(
+            &format!("SELECT {} FROM feeds
+                        WHERE url = $1", Self::props_str(""))));
+        let rows = try!(stmt.query(&[&url]));
+        let feeds = Feed::rows_to_items(rows);
+        if feeds.len() > 0 {
+            return Ok(feeds[0].clone());
         }
         return Err(Error::NotFound)
     }
 
-    pub fn find_all() -> Vec<Feed> {
-        let conn = conn().unwrap();
-        let stmt = conn.prepare("SELECT id, subscribers, title, description, language, velocity, website, status, curated, featured, last_updated, visual_url, icon_url, cover_url FROM feeds").unwrap();
-        let mut feeds = Vec::new();
-        for row in stmt.query(&[]).unwrap().iter() {
-            feeds.push(Feed {
-                    id: row.get(0),
-           subscribers: row.get(1),
-                 title: row.get(2),
-           description: row.get(3),
-              language: row.get(4),
-              velocity: row.get(5),
-               website: row.get(6),
-                topics: None,
-                status: row.get(7),
-               curated: row.get(8),
-              featured: row.get(9),
-          last_updated: row.get(10),
-            visual_url: row.get(11),
-              icon_url: row.get(12),
-             cover_url: row.get(13),
-            })
+    pub fn find_or_create_by_url(url: String) -> Result<Feed, Error> {
+        match Feed::find_by_url(&url) {
+            Ok(feed) => Ok(feed),
+            Err(_)   => Feed::create_by_url(url)
         }
-        return feeds
     }
 
-    pub fn create(feed: Feed) -> Result<(), Error> {
-        let conn = conn().unwrap();
-        let stmt = conn.prepare("INSERT INTO feeds (id, u) VALUES ($1)").unwrap();
-        let _ = try!(stmt.query(&[
-            &feed.id,
-            &feed.subscribers,
-            &feed.title,
-            &feed.description,
-            &feed.language,
-            &feed.velocity,
-            &feed.website,
-            &feed.topics,
-            &feed.status,
-            &feed.curated,
-            &feed.featured,
-            &feed.last_updated,
-            &feed.visual_url,
-            &feed.icon_url,
-            &feed.cover_url,
-        ]));
+    pub fn create_by_url(url: String) -> Result<Feed, Error> {
+        let conn = try!(conn());
+        let stmt = try!(conn.prepare("INSERT INTO feeds (url) VALUES ($1) RETURNING id"));
+        let rows = try!(stmt.query(&[&url]));
+        for row in rows.iter() {
+            let feed = Feed {
+                id:           row.get(0),
+                url:          url,
+                title:        "".to_string(),
+                description:  None,
+                language:     None,
+                velocity:     0.0,
+                website:      None,
+                state:        State::Alive,
+                last_updated: UTC::now().naive_utc(),
+                crawled:      UTC::now().naive_utc(),
+                visual_url:   None,
+                icon_url:     None,
+                cover_url:    None,
+                created_at:   UTC::now().naive_utc(),
+                updated_at:   UTC::now().naive_utc(),
+            };
+            return Ok(feed);
+        }
+        Err(Error::Unexpected)
+    }
+
+    pub fn fetch_props(&mut self) -> Result<(), Error> {
+        let rss_feed      = try!(rss::fetch(&self.url));
+        let now           = UTC::now().naive_utc();
+        self.title        = rss_feed.title.unwrap_or("".to_string());
+        self.description  = rss_feed.description;
+        self.language     = rss_feed.language;
+        self.website      = rss_feed.website;
+        self.last_updated = rss_feed.last_updated.unwrap_or(now);
+        self.crawled      = now;
+
+        self.visual_url   = rss_feed.visual_url;
+        self.icon_url     = rss_feed.icon_url;
+        self.cover_url    = rss_feed.cover_url;
         Ok(())
+    }
+
+    pub fn crawl(&mut self) -> Result<Vec<Entry>, Error> {
+        let rss_feed      = try!(rss::fetch(&self.url));
+        let mut entries   = vec![];
+        for entry in rss_feed.entries {
+            if entry.alternate.len() == 0 {
+                continue;
+            }
+            let alt = entry.alternate.first().unwrap();
+            if let Ok(_) = Entry::find_by_url(&alt.href) {
+                continue;
+            }
+            match Entry::create_by_url(alt.href.to_string()) {
+                Ok(mut e) => {
+                    println!("Found new entry: {}", e.url);
+                    e.update_with_feed_entry(&entry);
+                    e.feed_id = Some(self.id);
+                    try!(e.save());
+                    let _ = e.playlistify();
+                    if let Ok(_) = e.save() {
+                        entries.push(e);
+                    }
+                },
+                Err(_) => (),
+            }
+        }
+        Ok(entries)
     }
 }
