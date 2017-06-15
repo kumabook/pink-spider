3a726ef5875f4f160b258ca396bb1c6f4fd72024
Hiroki Kumamoto <kumabook@live.jp>
2017-06-15 15:46:56 +0900

    Update entry api client: Add show api and feed params to index api

--- web/js/api/entry.js
+++ web/js/api/entry.js
@@ -2,10 +2,18 @@ import axios              from 'axios';
 import { defaultPerPage } from '../config';
 
 export default {
-  index: (page = 0, perPage = defaultPerPage) => axios.get('/v1/entries', {
-    params: {
-      page,
-      per_page: perPage,
-    },
-  }).then(response => response.data),
+  show:  id => axios.get(`/v1/entries/${id}`).then(response => response.data),
+  index: (page = 0, perPage = defaultPerPage, feedId) => {
+    let path = '/v1/entries';
+    if (feedId) {
+      path = `/v1/feeds/${feedId}/entries`;
+    }
+    return axios.get(path, {
+      params: {
+        page,
+        per_page: perPage,
+      },
+    }).then(response => response.data);
+  },
+  update: entry => axios.post(`/v1/entries/${entry.id}`),
 };
