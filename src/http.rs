29b67269397ef4c02ac4da12ecdf078b2811b22a
Hiroki Kumamoto <kumabook@live.jp>
2017-11-12 21:51:44 +0900

    Set http client timeout

--- src/http.rs
+++ src/http.rs
@@ -1,8 +1,11 @@
 use hyper;
 use hyper_native_tls;
+use std::time::Duration;
 
 pub fn client() -> hyper::Client {
     let tls        = hyper_native_tls::NativeTlsClient::new().unwrap();
     let connector  = hyper::net::HttpsConnector::new(tls);
-    hyper::Client::with_connector(connector)
+    let mut client = hyper::Client::with_connector(connector);
+    client.set_read_timeout(Some(Duration::new(30, 0)));
+    client
 }
