189145b22c2b534cf827abf754170822a442bd0a
Hiroki Kumamoto <kumabook@live.jp>
2018-03-08 19:42:28 +0900

    Add query parameter to index APIs

--- web/js/api/artist.js
+++ web/js/api/artist.js
@@ -3,10 +3,11 @@ import { defaultPerPage } from '../config';
 
 export default {
   show:  id => axios.get(`/v1/artists/${id}`).then(response => response.data),
-  index: (page = 0, perPage = defaultPerPage) => axios.get('/v1/artists', {
+  index: (page = 0, perPage = defaultPerPage, query) => axios.get('/v1/artists', {
     params: {
       page,
       per_page: perPage,
+      query,
     },
   }).then(response => response.data),
   update: artist => axios.post(`/v1/artists/${artist.id}`),
--- web/js/api/feed.js
+++ web/js/api/feed.js
@@ -3,10 +3,11 @@ import { defaultPerPage } from '../config';
 
 export default {
   show:  id => axios.get(`/v1/feeds/${id}`).then(response => response.data),
-  index: (page = 0, perPage = defaultPerPage) => axios.get('/v1/feeds', {
+  index: (page = 0, perPage = defaultPerPage, query) => axios.get('/v1/feeds', {
     params: {
       page,
       per_page: perPage,
+      query,
     },
   }).then(response => response.data),
   update: feed => axios.post(`/v1/feeds/${feed.id}`),
