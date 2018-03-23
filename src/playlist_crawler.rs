a9c06648f60557e1921748561a8c0490efd40b83
Hiroki Kumamoto <kumabook@live.jp>
2018-03-23 19:02:06 +0900

    Better logging for playlist_crawler

--- src/playlist_crawler.rs
+++ src/playlist_crawler.rs
@@ -1,14 +1,21 @@
+extern crate chrono;
 extern crate pink_spider;
 
+use std::time::Instant;
+use chrono::Duration;
 use pink_spider::model::Playlist;
 
 pub fn main() {
     println!("Start crawling playlists...");
-
+    let now = Instant::now();
     let mut playlists = Playlist::find_actives();
     println!("{} playlists", playlists.len());
+    let total = playlists.len();
+    let mut index = 0;
     for playlist in playlists.iter_mut() {
-        println!("Fetch latest tracks of [{}] {}: {}",
+        println!("[{}/{}] Fetch latest tracks of [{}] {}: {}",
+                 index,
+                 total,
                  playlist.provider,
                  playlist.id,
                  playlist.title);
@@ -20,7 +27,9 @@ pub fn main() {
             }
         };
         println!("{} tracks are added", tracks.len());
+        index += 1;
     }
 
-    println!("Complete crawling playlists...");
+    println!("Complete crawling playlists... total {} ms",
+             Duration::from_std(now.elapsed()).unwrap().num_milliseconds());
 }
