003a863d74ec8eb3aa8187a2e8f60f6c6fe26fc7
Hiroki Kumamoto <kumabook@live.jp>
2018-03-23 19:01:46 +0900

    Better logging for crawler

--- src/crawler.rs
+++ src/crawler.rs
@@ -4,20 +4,23 @@ extern crate hyper;
 extern crate chrono;
 extern crate serde_json;
 
+use std::time::Instant;
+use chrono::Duration;
 use pink_spider::model::{Model, Feed};
 
 pub fn main() {
     println!("Start crawling...");
-
+    let now = Instant::now();
     let mut page  = 0;
     let per_page  = 10;
     let mut feeds = Feed::find(0, 0, None);
     let total = feeds.total;
+    let mut index = 0;
     println!("{} feeds", total);
     while feeds.page * feeds.per_page < total {
         feeds = Feed::find(page, per_page, None);
         for mut feed in feeds.items {
-            println!("Crawl {} ", feed.url);
+            println!("[{}/{}] Crawl {} ", index, total, feed.url);
             match feed.crawl() {
                 Ok(entries) => {
                     println!("Found {} entries", entries.len());
@@ -32,8 +35,10 @@ pub fn main() {
                     println!("Failed to crawl {}: {:?}", feed.url, e);
                 },
             }
+            index += 1;
         }
         page += 1;
     }
-    println!("Complete crawling...");
+    println!("Complete crawling... total {} ms",
+             Duration::from_std(now.elapsed()).unwrap().num_milliseconds());
 }
