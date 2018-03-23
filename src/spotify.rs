5816064f0d0be38417b639f19840ca7e94449534
Hiroki Kumamoto <kumabook@live.jp>
2018-03-23 18:56:45 +0900

    Fix bug of spotify::PagingObject#fetch_next

--- src/spotify.rs
+++ src/spotify.rs
@@ -169,14 +169,17 @@ pub struct PagingObject<T> {
 impl<T: DeserializeOwned> PagingObject<T> {
     pub fn fetch_next(&self) -> serde_json::Result<PagingObject<T>> {
         if let Some(ref path) = self.next {
-            fetch(&path).and_then(|s| serde_json::from_str(&s))
+            fetch_url(&path).and_then(|s| {
+                println!("{}", s);
+                serde_json::from_str(&s)
+            })
         } else {
             Err(serde_json::Error::missing_field("no value"))
         }
     }
     pub fn fetch_previous(&self) -> serde_json::Result<PagingObject<T>> {
         if let Some(ref path) = self.next {
-            fetch(&path).and_then(|s| serde_json::from_str(&s))
+            fetch_url(&path).and_then(|s| serde_json::from_str(&s))
         } else {
             Err(serde_json::Error::missing_field("no value"))
         }
