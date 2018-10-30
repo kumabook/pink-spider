6ce1e501a4ebb455f91eb02755f19e041fdc1668
Hiroki Kumamoto <kumabook@live.jp>
2018-10-30 09:17:32 +0900

    Add prefix to rss_crawler

--- src/crawler.rs
+++ src/crawler.rs
@@ -9,36 +9,36 @@ use chrono::Duration;
 use pink_spider::model::{Model, Feed};
 
 pub fn main() {
-    println!("Start crawling...");
+    println!("[rss_cralwer] Start crawling...");
     let now = Instant::now();
     let mut page  = 0;
     let per_page  = 10;
     let mut feeds = Feed::find(0, 0, None);
     let total = feeds.total;
     let mut index = 0;
-    println!("{} feeds", total);
+    println!("[rss_cralwer] {} feeds", total);
     while feeds.page * feeds.per_page < total {
         feeds = Feed::find(page, per_page, None);
         for mut feed in feeds.items {
-            println!("[{}/{}] Crawl {} ", index, total, feed.url);
+            println!("[rss_cralwer][{}/{}] Crawl {} ", index, total, feed.url);
             match feed.crawl() {
                 Ok(entries) => {
-                    println!("Found {} entries", entries.len());
-                    println!("Found {} tracks",
+                    println!("[rss_cralwer] Found {} entries", entries.len());
+                    println!("[rss_cralwer] Found {} tracks",
                              entries.iter().fold(0, |sum, e| sum + e.tracks.len()));
-                    println!("Found {} albums",
+                    println!("[rss_cralwer] Found {} albums",
                              entries.iter().fold(0, |sum, e| sum + e.albums.len()));
-                    println!("Found {} playlists",
+                    println!("[rss_cralwer] Found {} playlists",
                              entries.iter().fold(0, |sum, e| sum + e.playlists.len()));
                 },
                 Err(e) => {
-                    println!("Failed to crawl {}: {:?}", feed.url, e);
+                    println!("[rss_cralwer] Failed to crawl {}: {:?}", feed.url, e);
                 },
             }
             index += 1;
         }
         page += 1;
     }
-    println!("Complete crawling... total {} ms",
+    println!("[rss_cralwer] Complete crawling... total {} ms",
              Duration::from_std(now.elapsed()).unwrap().num_milliseconds());
 }
